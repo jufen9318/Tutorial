9e0ad3ba3cb2efcac6e2763901583298751ad385
jufeng <723368311@qq.com>
2020-05-14 21:01:15 +0800

    sql

---
+++ sq.sql
@@ -0,0 +1,10 @@
+SELECT * FROM quants_business.sheet1;
+
+
+select sum(成交金额) from quants_business.sheet1 where 买卖方向='买入';
+select sum(成交金额) from quants_business.sheet1 where 买卖方向='卖出';
+
+
+
+132471
+317192.2
